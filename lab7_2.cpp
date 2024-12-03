bf7c2c69e105b48f4c9f4784b29c4e46907b52d2
Tenten <pakpichamoon@gmail.com>
2024-12-03 16:09:07 +0700

    1st attempt

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,58 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n" ;
+    cout << "?????: " ;
+    getline(cin,name) ;
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." ;
+
+    int rahas ;
+    cout << "\nFahsai: I think you are an Engineering student. What is your student ID?\n" ;
+    cout << name << ": " ;
+    cin >> rahas ;
+    cout << "Fahsai: I think you may be GEAR " << int (rahas/pow(10,7) - 12) ;
+    cout << ". I have a free movie ticket for you.\nFahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n" ;
+    cout << name << ": " ;
+    string movie ;
+    cin.ignore() ;
+    getline(cin,movie) ;
+    cout << "Fahsai: So....which day are you free to go with me?\n" ;
+    
+    string day ;
+    cout << name << ": " ;
+    getline(cin,day) ;
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n" ;
+    string whatever ;
+    cout << name << ": " ;
+    cin >> whatever ;
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \(^ ^)/" ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
